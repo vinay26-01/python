8a75dc80d338b537763fcb9595afff417026af64
vinay26-01 <84130823+vinay26-01@users.noreply.github.com>
2021-05-14 21:15:33 +0530

    Update max digit count and min digit count.py

--- max digit count and min digit count.py
+++ max digit count and min digit count.py
@@ -1,15 +1,15 @@
-num=int(input("enter number = ")) #1231
-max,min=num%10,num%10 #1,1
+num=int(input("enter number = ")) 
+max,min=num%10,num%10 
 maximumcount=0
 minimumcount=0
-while num: #1231 #123 #12
-    r=num%10 #1 #3 #2
-    num=num//10 #123 #12 #1
-    if max<r: #1<1 (false) #3<12 #3<2(false)
-        max=r #3
-        maximumcount=1 #1
-    elif max==r: #1==r=1 #3==2(false)
-         maximumcount+=1 #2
+while num: 
+    r=num%10 
+    num=num//10 
+    if max<r: 
+        max=r 
+        maximumcount=1 
+    elif max==r: #1==r=1 
+         maximumcount+=1 
     if min>r:#1>2 (false)
         minimumcount=1
         min=r
